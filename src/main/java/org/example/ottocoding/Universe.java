a8130933342d0c553e82ac87a61e5445179ef1ad
Philipp Middendorf <middendorf@plapadoo.de>
2020-03-18 10:55:16 +0100

    ✨ Implement all the exercises and add some documentation

--- src/main/java/org/example/ottocoding/Universe.java
+++ src/main/java/org/example/ottocoding/Universe.java
@@ -10,33 +10,93 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleDirectedWeightedGraph;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Universe {
+public final class Universe {
+  private final Graph<StarSystem, DefaultWeightedEdge> graph;
 
-  public static Graph<StarSystem, DefaultWeightedEdge> createUniverse() {
-    Graph<StarSystem, DefaultWeightedEdge> g = new SimpleDirectedWeightedGraph<StarSystem, DefaultWeightedEdge>(
-            DefaultWeightedEdge.class);
-    g.addVertex(SOLAR_SYSTEM);
-    g.addVertex(ALPHA_CENTAURI);
-    g.addVertex(SIRIUS);
-    g.addVertex(BETELGEUSE);
-    g.addVertex(VEGA);
-    g.setEdgeWeight(g.addEdge(SOLAR_SYSTEM, ALPHA_CENTAURI), 5.0);
-    g.setEdgeWeight(g.addEdge(ALPHA_CENTAURI, SIRIUS), 4.0);
-    g.setEdgeWeight(g.addEdge(SIRIUS, BETELGEUSE), 8.0);
-    g.setEdgeWeight(g.addEdge(BETELGEUSE, SIRIUS), 8.0);
-    g.setEdgeWeight(g.addEdge(BETELGEUSE, VEGA), 6.0);
-    g.setEdgeWeight(g.addEdge(SOLAR_SYSTEM, BETELGEUSE), 5.0);
-    g.setEdgeWeight(g.addEdge(SIRIUS, VEGA), 2.0);
-    g.setEdgeWeight(g.addEdge(VEGA, ALPHA_CENTAURI), 3.0);
-    g.setEdgeWeight(g.addEdge(SOLAR_SYSTEM, VEGA), 7.0);
-    return g;
+  public Universe() {
+    this.graph = new SimpleDirectedWeightedGraph<>(DefaultWeightedEdge.class);
+    this.graph.addVertex(SOLAR_SYSTEM);
+    this.graph.addVertex(ALPHA_CENTAURI);
+    this.graph.addVertex(SIRIUS);
+    this.graph.addVertex(BETELGEUSE);
+    this.graph.addVertex(VEGA);
   }
 
+  public void addEdge(final StarSystem from, final StarSystem to, final double travelTime) {
+    this.graph.setEdgeWeight(this.graph.addEdge(from, to), travelTime);
+  }
+
+  /**
+   * Create the universe given in the task description
+   *
+   * @return See above
+   */
+  public static Universe createUniverse() {
+    final Universe result = new Universe();
+    result.addEdge(SOLAR_SYSTEM, ALPHA_CENTAURI, 5.0);
+    result.addEdge(ALPHA_CENTAURI, SIRIUS, 4.0);
+    result.addEdge(SIRIUS, BETELGEUSE, 8.0);
+    result.addEdge(BETELGEUSE, SIRIUS, 8.0);
+    result.addEdge(BETELGEUSE, VEGA, 6.0);
+    result.addEdge(SOLAR_SYSTEM, BETELGEUSE, 5.0);
+    result.addEdge(SIRIUS, VEGA, 2.0);
+    result.addEdge(VEGA, ALPHA_CENTAURI, 3.0);
+    result.addEdge(SOLAR_SYSTEM, VEGA, 7.0);
+    return result;
+  }
+
+  public Graph<StarSystem, DefaultWeightedEdge> getGraph() {
+    return graph;
+  }
+
+  private List<List<StarSystem>> extendRoutes(
+          final List<StarSystem> currentRoute, final Predicate<List<StarSystem>> stopPredicate) {
+    // So we can find an edge "from" something later on
+    assert !currentRoute.isEmpty();
+    final List<List<StarSystem>> result = new ArrayList<>();
+    // We could allow the route with a single node, but it doesn't make that much sense
+    if (currentRoute.size() > 1)
+      result.add(currentRoute);
+    for (final DefaultWeightedEdge e : graph.outgoingEdgesOf(currentRoute.get(currentRoute.size() - 1))) {
+      final List<StarSystem> newResult = new ArrayList<>(currentRoute);
+      newResult.add(this.graph.getEdgeTarget(e));
+      if (!stopPredicate.test(newResult))
+        result.addAll(extendRoutes(newResult, stopPredicate));
+    }
+    return result;
+  }
+
+  /**
+   * Calculate all possible routes, starting at a star system and applying a stop predicate so we don't have an infinite loop
+   * @param start Starting star system
+   * @param stopPredicate When to stop extending a route
+   * @param filterPredicate Filter for the resulting routes
+   * @return See above
+   */
+  public List<List<StarSystem>> allRoutes(
+          final StarSystem start,
+          final Predicate<List<StarSystem>> stopPredicate,
+          final Predicate<List<StarSystem>> filterPredicate) {
+    return extendRoutes(Collections.singletonList(start), stopPredicate).stream()
+                                                                        .filter(filterPredicate)
+                                                                        .collect(Collectors.toList());
+  }
+
+  /**
+   * Helper function to generate a sequence of adjacent pairs from an iterable
+   *
+   * @param iterable An iterable
+   * @param <A>      Value type of the iterable
+   * @return A stream of adjacent pairs (can be empty if the iterable doesn't have at least two elements)
+   */
   private static <A> Stream<Pair<A, A>> pairStream(final Iterable<A> iterable) {
     Iterator<A> it = iterable.iterator();
     if (!it.hasNext())
@@ -51,12 +111,19 @@ public class Universe {
     return builder.build();
   }
 
-  public static double routeDistance(final Graph<StarSystem, DefaultWeightedEdge> g, final List<StarSystem> stations) {
+  /**
+   * Given a list of waypoints, calculate the total travel distance on that route
+   *
+   * @param stations Intermediate stations
+   * @return The sum of the edge weights
+   * @throws NoSuchRouteException if we cannot find a route between two star systems
+   */
+  public double routeDistance(final List<StarSystem> stations) {
     return pairStream(stations).mapToDouble(p -> {
-      final DefaultWeightedEdge e = g.getEdge(p.getA(), p.getB());
+      final DefaultWeightedEdge e = this.graph.getEdge(p.getA(), p.getB());
       if (e == null)
         throw new NoSuchRouteException(p.getA(), p.getB());
-      return g.getEdgeWeight(e);
+      return this.graph.getEdgeWeight(e);
     }).sum();
   }
 }
--- src/test/java/org/example/ottocoding/UniverseTest.java
+++ src/test/java/org/example/ottocoding/UniverseTest.java
@@ -10,25 +10,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.jgrapht.Graph;
-import org.jgrapht.GraphPath;
 import org.jgrapht.alg.cycle.HawickJamesSimpleCycles;
-import org.jgrapht.alg.shortestpath.AllDirectedPaths;
 import org.jgrapht.alg.shortestpath.BFSShortestPath;
-import org.jgrapht.graph.DefaultWeightedEdge;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 class UniverseTest {
-  private final Graph<StarSystem, DefaultWeightedEdge> universe = createUniverse();
+  private final Universe universe = createUniverse();
 
+  // A little helper function to reduce duplication in the tests
   private void assertRouteDistance(final double d, final List<StarSystem> stations) {
-    assertEquals(d, Universe.routeDistance(universe, stations), 0.001);
+    assertEquals(d, universe.routeDistance(stations), 0.001);
   }
 
   @Test
@@ -53,51 +49,72 @@ class UniverseTest {
 
   @Test
   void exercise5() {
-    assertThrows(NoSuchRouteException.class,
-            () -> Universe.routeDistance(universe, List.of(SOLAR_SYSTEM, VEGA, BETELGEUSE)));
+    assertThrows(NoSuchRouteException.class, () -> universe.routeDistance(List.of(SOLAR_SYSTEM, VEGA, BETELGEUSE)));
   }
 
   @Test
   void exercise6() {
-    final List<List<StarSystem>> cycles = new HawickJamesSimpleCycles<>(universe).findSimpleCycles()
-                                                                                 .stream()
-                                                                                 .filter(l -> l.contains(SIRIUS))
-                                                                                 .filter(l -> l.size() <= 3)
-                                                                                 .collect(Collectors.toList());
-    assertTrue(cycles.contains(List.of(VEGA, SIRIUS, ALPHA_CENTAURI)));
-    assertTrue(cycles.contains(List.of(BETELGEUSE, SIRIUS)));
+    final List<List<StarSystem>> paths = this.universe.allRoutes(SIRIUS,
+            (route) -> route.size() > 4,
+            (route) -> route.get(route.size() - 1) == SIRIUS);
+    assertEquals(2, paths.size());
+    assertTrue(paths.contains(List.of(SIRIUS, VEGA, ALPHA_CENTAURI, SIRIUS)));
+    assertTrue(paths.contains(List.of(SIRIUS, BETELGEUSE, SIRIUS)));
   }
 
   @Test
   void exercise7() {
-    final List<List<StarSystem>> paths = new AllDirectedPaths<>(universe).getAllPaths(SOLAR_SYSTEM, SIRIUS, false, 3)
-                                                                         .stream()
-                                                                         .map(GraphPath::getVertexList)
-                                                                         // we can extend paths of size 3 by one hop
-                                                                         .filter(vl -> vl.size() == 4 || vl.size() == 3)
-                                                                         .collect(Collectors.toList());
-    assertTrue(paths.contains(List.of(SOLAR_SYSTEM, ALPHA_CENTAURI, SIRIUS)));
-    assertTrue(paths.contains(List.of(SOLAR_SYSTEM, BETELGEUSE, SIRIUS)));
-    assertTrue(paths.contains(List.of(SOLAR_SYSTEM, VEGA, ALPHA_CENTAURI, SIRIUS)));
+    final List<List<StarSystem>> paths = this.universe.allRoutes(SOLAR_SYSTEM,
+            (route) -> route.size() > 5,
+            (route) -> route.get(route.size() - 1) == SIRIUS && route.size() == 5);
+    assertEquals(3, paths.size());
+    assertTrue(paths.contains(List.of(SOLAR_SYSTEM, ALPHA_CENTAURI, SIRIUS, BETELGEUSE, SIRIUS)));
+    assertTrue(paths.contains(List.of(SOLAR_SYSTEM, BETELGEUSE, SIRIUS, BETELGEUSE, SIRIUS)));
+    assertTrue(paths.contains(List.of(SOLAR_SYSTEM, BETELGEUSE, VEGA, ALPHA_CENTAURI, SIRIUS)));
   }
 
   @Test
   void exercise8() {
-    assertEquals(9.0, new BFSShortestPath<>(universe).getPathWeight(SOLAR_SYSTEM, SIRIUS), 0.001);
+    assertEquals(9.0, new BFSShortestPath<>(universe.getGraph()).getPathWeight(SOLAR_SYSTEM, SIRIUS), 0.001);
   }
 
   @Test
   void exercise9() {
-    final Optional<List<StarSystem>> cycles = new HawickJamesSimpleCycles<>(universe).findSimpleCycles()
-                                                                                     .stream()
-                                                                                     .filter(l -> l.contains(
-                                                                                             ALPHA_CENTAURI))
-                                                                                     .peek(Collections::reverse)
-                                                                                     .peek(l -> l.add(l.get(0)))
-                                                                                     .min(Comparator.comparing(cycle -> Universe
-                                                                                             .routeDistance(universe,
-                                                                                                     cycle)));
+    // Find all cycles, take the one containing alpha centauri with the smallest travel time.
+    final Optional<List<StarSystem>> cycles = new HawickJamesSimpleCycles<>(universe.getGraph()).findSimpleCycles()
+                                                                                                .stream()
+                                                                                                .filter(l -> l.contains(
+                                                                                                        ALPHA_CENTAURI))
+                                                                                                .peek(Collections::reverse)
+                                                                                                .peek(l -> l.add(l.get(0)))
+                                                                                                .min(Comparator.comparing(
+                                                                                                        universe::routeDistance));
     assertTrue(cycles.isPresent());
-    assertEquals(9.0, Universe.routeDistance(universe, cycles.get()), 0.001);
+    assertEquals(9.0, universe.routeDistance(cycles.get()), 0.001);
+  }
+
+  @Test
+  void exercise10() {
+    final List<List<StarSystem>> paths = this.universe.allRoutes(SIRIUS,
+            (route) -> universe.routeDistance(route) >= 30,
+            (route) -> route.get(route.size() - 1) == SIRIUS);
+    assertEquals(7, paths.size());
+    assertTrue(paths.contains(List.of(SIRIUS, BETELGEUSE, SIRIUS)));
+    assertTrue(paths.contains(List.of(SIRIUS, VEGA, ALPHA_CENTAURI, SIRIUS)));
+    assertTrue(paths.contains(List.of(SIRIUS, VEGA, ALPHA_CENTAURI, SIRIUS, BETELGEUSE, SIRIUS)));
+    assertTrue(paths.contains(List.of(SIRIUS, BETELGEUSE, SIRIUS, VEGA, ALPHA_CENTAURI, SIRIUS)));
+    assertTrue(paths.contains(List.of(SIRIUS, BETELGEUSE, VEGA, ALPHA_CENTAURI, SIRIUS)));
+    assertTrue(paths.contains(List.of(SIRIUS, VEGA, ALPHA_CENTAURI, SIRIUS, VEGA, ALPHA_CENTAURI, SIRIUS)));
+    assertTrue(paths.contains(List.of(
+            SIRIUS,
+            VEGA,
+            ALPHA_CENTAURI,
+            SIRIUS,
+            VEGA,
+            ALPHA_CENTAURI,
+            SIRIUS,
+            VEGA,
+            ALPHA_CENTAURI,
+            SIRIUS)));
   }
 }
